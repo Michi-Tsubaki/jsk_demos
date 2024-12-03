b8b0d9cfef2dd1d20f560cf1a7a7eeab6e60ed49
Michi-Tsubaki <tsubaki@g.ecc.u-tokyo.ac.jp>
2024-12-04 00:09:33 +0900

    try to make ros pkg, but...

---
+++ jsk_2024_10_semi/pr2_surgery/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mech-user/semi_ws/src/jsk_2024_10_semi/pr2_surgery
+# Build directory: /home/mech-user/semi_ws/src/jsk_2024_10_semi/pr2_surgery/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
